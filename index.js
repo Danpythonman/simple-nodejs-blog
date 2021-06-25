9ee1bf4e7c203268ab3f13f3a3f7f084e765bc1b
Daniel Di Giovanni <dannyjdigio@gmail.com>
2021-06-25 01:43:50 -0400

    Added code to connect to database and add a document to a collection

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const fs = require("fs");
+const { MongoClient } = require("mongodb");
 
+// Start express
 const app = express();
 
+// Include static files in app
 app.use(express.static("static"));
+
+// MongoDB connection URI
+const URI = JSON.parse(fs.readFileSync("database-user.json"))["uri"];
+
+// Connect to database
+const mongoClient = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/", (req, res) => {
     res.sendFile("static/html/index.html", {root: __dirname});
@@ -10,6 +20,22 @@ app.get("/", (req, res) => {
 
 app.get("/getText", (req, res) => {
     res.send("Hi");
+
+    mongoClient.connect((err, db) => {
+        const collection = mongoClient.db(
+            JSON.parse(fs.readFileSync("database-user.json"))["database-name"]).collection(
+            JSON.parse(fs.readFileSync("database-user.json"))["collection-name"]);
+
+        objectToInsert = {title: "Manually Added Post Directly to Database", body: "Ha, I added this post directly to the database instead of having some programming logic."};
+
+        collection.insertOne(objectToInsert, (err, res) => {
+            if (err) {
+                throw err;
+            }
+            console.log("Document inserted");
+            mongoClient.close();
+        });
+    });
 });
 
 const PORT = 5000;
